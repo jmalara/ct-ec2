9ed7cef50f0793ee7437b52c63cd5e92db376f6e
Jeremy Malara <jmalara@beachbody.com>
2018-05-12 02:07:38 -0700

    adding

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# ct-ec2
-CT Assessment using EC2
+# Serverless example for CT
+This code deploys an sample ec2 server stack based on your requirements.  Tagging could be better and multi-region support in the CF isn't there, but you get the idea.
+
+# Requirements
+1. AWS CLI
+2. Access Key and Secret key for an administrator setup in your aws credential file under the default profile
+
+# Usage
+1. Clone the repository
+2. CD into the repository directory
+3. Run ./create.sh
+4. Wait for stack to complete and it will spit out the load balancer URL
+5. Profit
---
+++ create.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Way too simple serverless stack builder, ideally this would be called with Jenkins, CodeDeploy, etc but we'll use HumanCD here
+RANDO=$(awk -v min=1000 -v max=9999 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+
+# Checking for AWS CLI
+command -v aws >/dev/null 2>&1 || { echo >&2 "ERROR: AWS CLI was not found. Please install or ensure that it's in your PATH."; exit 1; }
+
+# Check for access
+aws iam get-user | grep 'error' &> /dev/null
+if [ $? == 0 ]; then
+   echo "ERROR: Your default AWS profile doesn't have the correct permissions. Make sure you have an account with the Administrator role and you've run aws configure."
+   exit 1
+fi
+
+echo "Building cloudformation stack..."
+# Deploy stack
+aws cloudformation create-stack --region us-west-2 --stack-name ct-stack-$RANDO --template-body file://cf/consumertrak.template &> /dev/null
+sleep 5s
+# This is a hack but it works for this manual scenario
+# This stack takes about 200 seconds to build so lets wait for that then keep trying to get the lb DNS
+echo "Waiting for stack to complete..."
+x=7
+while [ $x -gt 0 ]
+do
+    sleep 30s
+    echo "Waiting for stack to complete..."
+    x=$(( $x - 1 ))
+done
+
+# OK Stack should be ready now, lets try and get that lb DNS
+x=20
+while [ $x -gt 0 ]
+do
+    aws cloudformation --region us-west-2 describe-stacks --stack-name ct-stack-$RANDO  | grep 'CREATE_COMPLETE' &> /dev/null
+    if [ $? == 0 ]; then
+        # OK Stack is finally complete
+        URL=$(aws cloudformation --region us-west-2 describe-stacks --stack-name ct-stack4443  | grep 'OutputValue' |  sed "s/\"//g" | sed "s/,//g" |  awk '{print "http://"$2}')
+        printf "\nYou can access the website at the URL below\n"
+        echo $URL
+        exit 0
+    fi
+    sleep 5s
+    x=$(( $x - 1 ))
+done
+
+echo "There was an error creating the stack, oops"
+exit 1
